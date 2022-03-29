00e3659b826ebc927672b38dac5f2407d7ffcb97
Facundo Barrera <barreraaybarfacundo@gmail.com>
2022-03-29 20:41:30 -0300

    Refactor game to adapt to buttons
    Restructure the game in order to comply with TOD exercise
    
    The game can now be play first-to-five style and with a GUI, random dialog shows during the game as well as points

--- script.js
+++ script.js
@@ -1,81 +1,55 @@
-function game()
+// HTML elements
+const displayScore = document.querySelector(".title p");
+const displayDialog = document.querySelector(".hint");
+
+const startButton = document.querySelector(".start-button");
+const rockButton = document.querySelector("#button-rock");
+const paperButton = document.querySelector("#button-paper");
+const scissorsButton = document.querySelector("#button-scissors");
+
+// Game functions
+
+const computerWinDialog = ["HA! I am better than you.", "Is that all you got?"];
+const computerLostDialog = ["Pff... You just got lucky.", "You probably looked at the debbugger...", "It was the coder's fault!"];
+const computerDrawDialog = ["Just a mishap...", "Well... This isn't fun", "Nothing to see here"];
+
+let points = [0, 0]; //[user-points, computer-points]
+
+function game(userSelection)
 {
-    let computerPoints = 0, userPoints = 0;
-    let computerSelection = "", userSelection = "";
-    let winner;
-
-    console.log("The game is starting!");
-
-    // 5 round game
-    /* for (let round = 1; round <= 5; round++)
+    // Play the game
+    let roundWinner = playRound(computerPlay(), userSelection);
+    if (roundWinner === "user")
     {
-        console.log(`Round ${round}:`);
-
-        computerSelection = computerPlay();
-        userSelection = userPlay();
-
-        if (userSelection === "cancel")
-        {
-            console.log("User ended the game :(");
-            return;
-        }
-
-        winner = playRound(computerSelection, userSelection);
-
-        if (winner === "draw")
-        {
-            userPoints++;
-            computerPoints++;
-            console.log("It's a draw! Point for everyone :)");
-        }
-        else if (winner === "user")
-        {
-            userPoints++;
-            console.log(`You win! ${userSelection} beats ${computerSelection}`);
-        }
-        else
-        {
-            computerPoints++;
-            console.log(`You lose! ${computerSelection} beats ${userSelection}`);
-        }
-    } */
-
-    console.log(`The game ended! Points:\n\nUser: ${userPoints}\nComputer: ${computerPoints}`);
-    console.log((computerPoints === userPoints) ? "It's a draw!" :
-                (computerPoints > userPoints) ? "The computer wins!" : "The user wins!");
-}
-
-function computerPlay()
-{
-    // 1: Rock, 2: Paper, 3: Scissors
-
-    let random = getRandomInt(1,4); // 1 inclusive, 4 exclusive
-
-    return  (random === 1) ? 'Rock' :
-            (random === 2) ? 'Paper' :
-            (random === 3) ? 'Scissors' :
-            undefined;
-}
-
-function userPlay()
-{
-    let userSelection;
-
-    while (true)
+        points[0]++;
+        displayDialog.textContent = computerLostDialog[getRandomInt(0, computerLostDialog.length)];
+    }
+    else if (roundWinner === "computer")
     {
-        userSelection = prompt("Select rock, paper, or scissors:");
+        points[1]++;
+        displayDialog.textContent = computerWinDialog[getRandomInt(0, computerWinDialog.length)];
+    }
+    else
+    {
+        displayDialog.textContent = computerDrawDialog[getRandomInt(0, computerDrawDialog.length)];
+    }
 
-        if (userSelection === null)
-            return "cancel";
+    displayScore.innerHTML = `Score<br>User:${points[0]} - Computer:${points[1]}`;
 
-        userSelection = userSelection.toLowerCase();
-        if (!(userSelection === 'rock' || userSelection === 'paper' || userSelection === 'scissors'))
-            console.log("The selected value should be rock, paper, or scissors! Try again.");
+    // Check if match ended
+    if (points[0] === 5 || points[1] === 5)
+    {
+        if (points[0] === 5)
+            displayScore.textContent = "You win!";
         else
-        {
-            userSelection = userSelection.charAt(0).toUpperCase() + (userSelection.slice(1));
-            return userSelection;
-        }
+            displayScore.textContent = "You lose!";
+       
+        startButton.classList.toggle('hidden');
+        rockButton.classList.toggle('hidden');
+        paperButton.classList.toggle('hidden');
+        scissorsButton.classList.toggle('hidden');
+
+        return;
     }
 }
 
@@ -92,9 +66,40 @@ function playRound(computerSelection, userSelection)
     return winner;
 }
 
+function computerPlay()
+{
+    // 1: Rock, 2: Paper, 3: Scissors
+
+    let random = getRandomInt(1,4); // 1 inclusive, 4 exclusive
+
+    return  (random === 1) ? 'Rock' :
+            (random === 2) ? 'Paper' :
+            (random === 3) ? 'Scissors' :
+            undefined;
+}
+
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-let button = document.querySelector(".start-button");
-button.addEventListener('click', game);
+// Event addition
+startButton.addEventListener('click', () => {
+    startButton.classList.toggle('hidden');
+    rockButton.classList.toggle('hidden');
+    paperButton.classList.toggle('hidden');
+    scissorsButton.classList.toggle('hidden');
+
+    points = [0, 0];
+
+    displayDialog.textContent = "I have made my choice! Your turn...";
+});
+rockButton.addEventListener('click', () => {
+    game("Rock");
+});
+paperButton.addEventListener('click', () => {
+    game("Paper");
+});
+scissorsButton.addEventListener('click', () => {
+    game("Scissors");
+});
+
